37f395730ee1e63f88267183200a0acc39c53830
wallenjos01 <wallenjos01@gmail.com>
2024-05-24 15:55:50 -0600

    Don't force java 17 compiler

--- gradle/plugins/src/main/kotlin/mcping-build.gradle.kts
+++ gradle/plugins/src/main/kotlin/mcping-build.gradle.kts
@@ -4,7 +4,8 @@ plugins {
 }
 
 java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
     withSourcesJar()
 }
 
